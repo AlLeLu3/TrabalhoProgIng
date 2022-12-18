2d5ced5453fb52425e5d329e2d83af068e4e4506
AlLeLu3 <118358848+AlLeLu3@users.noreply.github.com>
2022-12-18 11:19:16 -0300

    Update index.php

--- index.php
+++ index.php
@@ -24,8 +24,8 @@ if(isset($_POST['botao'])){
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <title>Login</title>
 </head>
