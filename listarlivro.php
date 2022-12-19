c3734e99af002ebb1dd103907376c819625321a1
AlLeLu3 <118358848+AlLeLu3@users.noreply.github.com>
2022-12-19 11:56:11 -0300

    Update listarlivro.php

--- listarlivro.php
+++ listarlivro.php
@@ -1,80 +1,83 @@
 <?php
 require 'dbcon.php';
+session_start();
+if(isset($_SESSION['idioma'])){
+    if($_SESSION['idioma']=='pt'){
+        require 'idioma_pt.php';        
+    }else{
+        require 'idioma_in.php';
+    }
+}else{
+    require 'idioma_in.php';
+}
 ?>
 <!doctype html>
 <html lang="pt-BR">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <title>Lista dos livros.</title>
+    <title><?=$texto['autor']?>.</title>
 </head>
 <body>
+    <div class="container mt-4">
 
-    <div class="container mt-5">
+        <?php include('mensagem.php'); ?>
 
         <div class="row">
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
-                        <h4>Dados do livro 
-                            <a href="restrita.php" class="btn btn-danger float-end">VOLTAR</a>
+                        <h4>
+                            <?=$texto['autores']?> 
+
+                            <a href="restrita.php" class="btn btn-danger float-end"><?=$texto['voltar']?></a>
+                            <a href="cadastrarautor.php" class="btn btn-primary float-end"><?=$texto['criarAutor']?></a>
                         </h4>
                     </div>
                     <div class="card-body">
 
-                        <?php
-                        if(isset($_GET['idLivro']))
-                        {
-                            $idLivro = mysqli_real_escape_string($con, $_GET['idLivro']);
-                            $query = "SELECT l.idLivro, l.Titulo, l.Genero, l.Ano, a.Nome FROM livro l, autor a WHERE l.idAutor=a.idAutor ORDER BY Titulo ASC";
-                            $query_run = mysqli_query($con, $query);
+                        <table class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th><?=$texto['autor']?></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php 
+                                    $query = "SELECT * FROM autor a ORDER BY Nome ASC";
+                                    $query_run = mysqli_query($con, $query);
 
-                            if(mysqli_num_rows($query_run) > 0)
-                            {
-                                $livro = mysqli_fetch_array($query_run);
+                                    if(mysqli_num_rows($query_run) > 0)
+                                    {
+                                        foreach($query_run as $autor)
+                                        {
+                                            ?>
+                                                <td><?= $autor['Nome']; ?></td>
+                                            </tr>
+                                            <?php
+                                        }
+                                    }
+                                    else
+                                    {
+                                        echo "<h5> {$texto['restritaSAutor']} </h5>";
+                                    }
                                 ?>
                                 
-                                    <div class="mb-3">
-                                        <label>Título</label>
-                                        <p class="form-control">
-                                            <?=$livro['Titulo'];?>
-                                        </p>
-                                    </div>
-                                    <div class="mb-3">
-                                        <label>Gênero</label>
-                                        <p class="form-control">
-                                            <?=$livro['Genero'];?>
-                                        </p>
-                                    </div>
-                                    <div class="mb-3">
-                                        <label>Ano</label>
-                                        <p class="form-control">
-                                            <?=$livro['Ano'];?>
-                                        </p>
-                                    </div>
-                                    <div class="mb-3">
-                                        <label>Autor</label>
-                                        <p class="form-control">
-                                            <?=$livro['Nome'];?>
-                                        </p>
-                                    </div>
+                            </tbody>
+                        </table>
 
-                                <?php
-                            }
-                            else
-                            {
-                                echo "<h4>Nenhum ID encontrado</h4>";
-                            }
-                        }
-                        ?>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
 </html>
+<?php
