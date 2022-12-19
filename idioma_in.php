4eb03787b8181f33035cec80bddec495a9845e0c
AlLeLu3 <118358848+AlLeLu3@users.noreply.github.com>
2022-12-19 11:57:17 -0300

    Update idioma_in.php

--- idioma_in.php
+++ idioma_in.php
@@ -1,19 +1,17 @@
 <?php
 
-//=$texto['titulo']
-
 /*  index  */
 $texto['login'] = "User login";
-$texto['emailins'] = "Institutional E-mail:";
+$texto['emailins'] = "E-mail:";
 $texto['senha'] = "Password:";
 $texto['logar'] = "LOGIN";
 $texto['primeiravez'] = "First time here?";
 $texto['contaacesso'] = "Create your account to have a full access to this site.";
 $texto['criarconta'] = "CREATE NEW ACCOUNT";
+$texto['criarcont'] = "Create an account";
 $texto['criar'] = "CREATE";
 $texto['voltar'] = "BACK";
 $texto['cancela'] = "CANCEL";
-
 
 /*  restrita  */
 $texto['sair'] = "LOGOUT";
@@ -21,6 +19,7 @@ $texto['livros'] = "Books";
 $texto['biblioteca'] = "LIBRARY";
 $texto['criarAutor'] = "Create author";
 $texto['criarLivro'] = "Create book";
+$texto['vautores'] = "View authors";
 $texto['titulo'] = "Title";
 $texto['genero'] = "Genre";
 $texto['ano'] = "Year";
@@ -28,15 +27,27 @@ $texto['autor'] = "Author";
 $texto['editar'] = "Edit";
 $texto['deletar'] = "Delete";
 $texto['restritaSLivro'] = "There are no registered books";
+$texto['autores'] = "Authors";
 
-/*cadastro livro*/
+
+/*cadastro livro */
 $texto['registrar'] = "REGISTER BOOK";
 $texto['registro'] = "SUBMIT";
+$texto['succadastrar'] = "Successfully registered book!";
+$texto['cadastrar'] = "Error! It was not possible to register the book";
 
 /*editar livro*/
 $texto['editarl'] = "EDIT BOOK";
 $texto['editarli'] = "Edit book";
+$texto['suceditar'] = "Successfully edited book!";
+$texto['editarr'] = "Error! It was not possible to edit the book";
 
-$texto['dev'] = "Developers";
+/*excluir*/
+$texto['sucexcluir'] = "Successfully deleted book!";
+$texto['excluir'] = "Error! It was not possible to delete the book";
+
+/*cadastrar autor*/
+$texto['sucautor'] = "Successfully registered author!";
+$texto['autorr'] = "Error! It was not possible to register the author";
 $texto[''] = "";
 $texto[''] = "";
